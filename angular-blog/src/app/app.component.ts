24f56cbcdeeb1850a86993a23b7fba13268be532
criarsite <oktuga@gmail.com>
2023-12-02 19:56:36 +0000

    Atualizando

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SmallCardComponent } from "./components/small-card/small-card.component";
-import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
-import { BigCardComponent } from "./components/big-card/big-card.component";
+import { HomeComponent } from "./pages/home/home.component";
  
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, SmallCardComponent, MenuBarComponent, MenuTitleComponent, BigCardComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
