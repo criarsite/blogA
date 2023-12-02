25cb843ac66367dfbfa4d3ad9ac89c9285835cb5
criarsite <oktuga@gmail.com>
2023-12-02 21:53:12 +0000

    Atualizando

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+
+ 
  
 @Component({
     selector: 'app-root',
