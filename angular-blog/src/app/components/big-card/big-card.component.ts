1d4b654ae2fddc6ef71ed3a15725c49e1e6baad1
criarsite <oktuga@gmail.com>
2023-12-02 20:48:33 +0000

    Atualizando

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+fotoCard:string='https://th.bing.com/th/id/R.bb79a099832ae287d3778941b59b5bc5?rik=%2fZCWjB56SYe%2few&pid=ImgRaw&r=0' 
+cardTitulo:string="Desenvolvedor Angular"
+url:string= "https://github.com/criarsite"
+cardDescricao:string="Crie aplicações web modernas e dinâmicas usando o framework Angular e outras tecnologias relacionadas, como TypeScript, Node.js ou outros..."
+data:string="02/12/2023"
 
+constructor(){
+
+}
+ngInit(){
+
+}
 }
--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+imgSmalCard:string= "https://th.bing.com/th/id/OIP.30aoNxlSnaYrLhBT0O1lzwHaE8?rs=1&pid=ImgDetMain"
+texto:string=" Desenvolvedor Golang: Desenvolvimento de APIs e WebApps usango golang e angular"
+altImage:string="golang Developer"
 }
